74eb9745115ad36822972ecd043e1534bda20430
PixelBoot <bootingpixels@gmail.com>
2023-09-09 10:51:50 +0200

    vim3: import u-boot bin

--- firmware.mk
+++ firmware.mk
@@ -1,9 +1,1 @@
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),vim3)
-
-RADIO_FILES := $(wildcard $(LOCAL_PATH)/radio/*)
-$(foreach f, $(notdir $(RADIO_FILES)), \
-    $(call add-radio-file,radio/$(f)))
-
-endif

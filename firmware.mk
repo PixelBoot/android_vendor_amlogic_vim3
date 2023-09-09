8279df2d10e77f0aa44f9f88c4f8688a67c8a535
PixelBoot <bootingpixels@gmail.com>
2023-09-09 09:56:22 +0200

    vim3: rebrand from radxa0

--- firmware.mk
+++ firmware.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),radxa0)
+ifeq ($(TARGET_DEVICE),vim3)
 
 RADIO_FILES := $(wildcard $(LOCAL_PATH)/radio/*)
 $(foreach f, $(notdir $(RADIO_FILES)), \
